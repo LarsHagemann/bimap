651bc6cb8247179838a818e4d1e6e899a3569633
Lars Hagemann <hagemann.l@outlook.com>
2019-03-24 20:37:24 +0100

    Update bimap.hpp

--- bimap.hpp
+++ bimap.hpp
@@ -3,20 +3,9 @@
 #include <map>
 #include <memory>
 
-/*
-(c) 2019 Lars Hagemann
-This file is part of the std-extension collection written by Lars Hagemann.
-You may use it in any way you want to.
-*/
-
 namespace stde
 {
-	/*
-	* bimap is a map where the key and the value is unique, thus one can
-	* search for keys and for values. This makes sense when you map
-	* ids to for example memory locations. With a bimap you can map every
-	* id to a pointer and you can map every pointer to exactly one id.
-	*/
+	using namespace std;
 	template<
 		class _KeyType,
 		class _ValueType,
@@ -35,6 +24,7 @@ namespace stde
 		using _Tree_Node = std::pair<key_type, value_type>;
 		using _RTree_Node = std::pair<rkey_type, rvalue_type>;
 	private:
+		// Test for the container types
 		static_assert(
 			is_same_v<key_type, typename _Tree::key_type>,
 			"bimap::key_type has to match _Container_Type::key_type!");
@@ -52,10 +42,10 @@ namespace stde
 		_RTree m_rtree;
 	public:
 		/* Iterator functions */
-		auto begin()	const 	{ return m_tree.cbegin(); }
-		auto begin() 			{ return m_tree.begin(); }
-		auto end  ()	const 	{ return m_tree.cbegin(); }
-		auto end  () 			{ return m_tree.end(); }
+		auto begin()	const	{ return m_tree.cbegin();	}
+		auto begin()			{ return m_tree.begin();	}
+		auto end  ()	const	{ return m_tree.cbegin();	}
+		auto end  ()			{ return m_tree.end();		}
 	public:
 		/* Modification functions */
 		void insert(_Tree_Node&& node)
@@ -63,17 +53,22 @@ namespace stde
 			m_tree.insert({ node.first, node.second });
 			m_rtree.insert({ node.second, node.first });
 		}
-		void insert(key_type&& key, value_type&& value)
+		void insert(const _Tree_Node& node)
+		{
+			m_tree.insert({ node.first, node.second });
+			m_rtree.insert({ node.second, node.first });
+		}
+		void insert(const key_type& key, const value_type& value)
 		{
 			insert({ key, value });
 		}
-		void erase_key(key_type&& key)
+		void erase_key(const key_type& key)
 		{
 			auto value = get_value(key);
 			m_tree.erase(key);
 			m_rtree.erase(value);
 		}
-		void erase_value(value_type&& value)
+		void erase_value(const value_type& value)
 		{
 			auto key = get_key(value);
 			m_tree.erase(key);
@@ -81,40 +76,37 @@ namespace stde
 		}
 	public:
 		/* Helper functions */
-		bool has_key(key_type&& key)
+		bool has_key(const key_type& key) const
 		{
 			return m_tree.find(key) != m_tree.end();
 		}
-		bool has_value(value_type&& value)
+		bool has_value(const value_type& value) const
 		{
 			return m_rtree.find(value) != m_rtree.end();
 		}
-		const value_type& get_value(key_type&& key)
+		const value_type& get_value(const key_type& key) const
 		{
 			return m_tree.at(key);
 		}
-		const key_type& get_key(value_type&& value)
+		const key_type& get_key(const value_type& value) const
 		{
 			return m_rtree.at(value);
 		}
-		size_t size()
+		size_t size() const
 		{
-			if (m_tree.size() != m_rtree.size())
+			if (m_tree.size() != m_rtree.size()) 
 				throw(std::exception("Tree sizes do not match!"));
 			return m_tree.size();
 		}
 	};
 
-	/*
-	* Wrapper for bimaps using std::unordered_map instead of std::map
-	*/
 	template<
 		class _KeyType,
 		class _ValueType>
-	using unordered_bimap = bimap<
-		_KeyType,
-		_ValueType,
-		std::unordered_map<_KeyType, _ValueType>,
-		std::unordered_map<_ValueType, _KeyType>>;
+		using unordered_bimap = bimap<
+			_KeyType, 
+			_ValueType,
+			std::unordered_map<_KeyType, _ValueType>,
+			std::unordered_map<_ValueType, _KeyType>>;
 
 }
