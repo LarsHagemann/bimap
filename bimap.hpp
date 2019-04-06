f224be98e9f3418008c30f6e7c60cde06d8845ca
Lars Hagemann <hagemann.l@outlook.com>
2019-04-06 13:17:11 +0200

    Memory and performace updates for stde::bimap

--- bimap.hpp
+++ bimap.hpp
@@ -1,26 +1,43 @@
 #pragma once
-#include <unordered_map>
-#include <map>
-#include <memory>
+
+#include <unordered_set>
+#include <set>
 
 namespace stde
 {
 	using namespace std;
+
 	template<
 		class _KeyType,
 		class _ValueType,
-		class _Container_Type = std::map<_KeyType, _ValueType>,
-		class _Reverse_Container_Type = std::map<_ValueType, _KeyType>>
-	class bimap
+		class _Key_Container = std::set<_KeyType>,
+		class _Mapped_Container = std::set<_ValueType>>
+		class bimap
 	{
 	public:
+		class const_iterator : public iterator<
+			std::bidirectional_iterator_tag,
+			std::pair<const _KeyType&, const _ValueType&>>
+		{
+		public:
+			typedef const_iterator self_type;
+			typedef pair<typename _Key_Container::iterator, typename _Mapped_Container::iterator> value_type;
+			typedef int difference_type;
+			explicit const_iterator(const value_type& value) : m_data(std::move(value)) { }
+			const self_type& operator++() { ++m_data.first; ++m_data.second; return *this; }
+			const value_type& operator*() { return m_data; }
+			bool operator==(const self_type& rhs) { return m_data.first == rhs.m_data.first; }
+			bool operator!=(const self_type& rhs) { return m_data.first != rhs.m_data.first; }
+		private:
+			value_type m_data;
+		};
 		using key_type = _KeyType;
 		using value_type = _ValueType;
 		using rkey_type = _ValueType;
 		using rvalue_type = _KeyType;
 	private:
-		using _Tree = _Container_Type;
-		using _RTree = _Reverse_Container_Type;
+		using _Tree = _Key_Container;
+		using _RTree = _Mapped_Container;
 		using _Tree_Node = std::pair<key_type, value_type>;
 		using _RTree_Node = std::pair<rkey_type, rvalue_type>;
 	private:
@@ -29,74 +46,103 @@ namespace stde
 			is_same_v<key_type, typename _Tree::key_type>,
 			"bimap::key_type has to match _Container_Type::key_type!");
 		static_assert(
-			is_same_v<value_type, typename _Tree::mapped_type>,
-			"bimap::value_type has to match _Container_Type::value_type!");
-		static_assert(
 			is_same_v<rkey_type, typename _RTree::key_type>,
 			"bimap::rkey_type has to match _Reverse_Container_Type::key_type!");
-		static_assert(
-			is_same_v<rvalue_type, typename _RTree::mapped_type>,
-			"bimap::rvalue_type has to match _Reverse_Container_Type::value_type!");
 	private:
-		_Tree m_tree;
-		_RTree m_rtree;
+		_Tree m_key_tree;
+		_RTree m_value_tree;
+	private:
+		/* Inner helper functions */
+		inline size_t index_of_key(const key_type& key)
+		{
+			const auto k_itr = m_key_tree.find(key);
+			return std::distance(m_key_tree.begin(), k_itr);
+		}
+		inline size_t index_of_value(const value_type& value)
+		{
+			const auto k_itr = m_value_tree.find(value);
+			return std::distance(m_value_tree.begin(), k_itr);
+		}
+		inline value_type value_at_index(size_t index)
+		{
+			auto v_itr = m_value_tree.begin();
+			std::advance(v_itr, index);
+			return *v_itr;
+		}
+		inline key_type key_at_index(size_t index)
+		{
+			auto k_itr = m_key_tree.begin();
+			std::advance(k_itr, index);
+			return *k_itr;
+		}
 	public:
 		/* Iterator functions */
-		auto begin()	const	{ return m_tree.cbegin();	}
-		auto begin()			{ return m_tree.begin();	}
-		auto end  ()	const	{ return m_tree.cend();		}
-		auto end  ()			{ return m_tree.end();		}
+		const_iterator	begin()	const { return const_iterator({ m_key_tree.begin(), m_value_tree.begin() }); }
+		const_iterator	end()	const { return const_iterator({ m_key_tree.end()  , m_value_tree.end()   }); }
 	public:
 		/* Modification functions */
 		void insert(_Tree_Node&& node)
 		{
-			m_tree.insert({ node.first, node.second });
-			m_rtree.insert({ node.second, node.first });
+			m_key_tree.emplace_hint(m_key_tree.begin(), std::move(node.first));
+			m_value_tree.emplace_hint(m_value_tree.begin(), std::move(node.second));
 		}
 		void insert(const _Tree_Node& node)
 		{
-			m_tree.insert({ node.first, node.second });
-			m_rtree.insert({ node.second, node.first });
+			m_key_tree.emplace_hint(m_key_tree.begin(), std::move(node.first));
+			m_value_tree.emplace_hint(m_value_tree.begin(), std::move(node.second));
 		}
 		void insert(const key_type& key, const value_type& value)
 		{
-			insert({ key, value });
+			m_key_tree.emplace_hint(m_key_tree.begin(), std::move(key));
+			m_value_tree.emplace_hint(m_value_tree.begin(), std::move(value));
 		}
 		void erase_key(const key_type& key)
 		{
-			auto value = get_value(key);
-			m_tree.erase(key);
-			m_rtree.erase(value);
+			const auto k_itr = m_key_tree.find(key);
+			const auto offset = std::distance(m_key_tree.begin(), k_itr);
+			auto v_itr = m_value_tree.begin();
+			std::advance(v_itr, offset);
+			m_key_tree.erase(k_itr);
+			m_value_tree.erase(v_itr);
 		}
 		void erase_value(const value_type& value)
 		{
-			auto key = get_key(value);
-			m_tree.erase(key);
-			m_rtree.erase(value);
+			const auto v_itr = m_value_tree.find(value);
+			const auto offset = std::distance(m_value_tree.begin(), v_itr);
+			auto k_itr = m_key_tree.begin();
+			std::advance(k_itr, offset);
+			m_key_tree.erase(k_itr);
+			m_value_tree.erase(v_itr);
 		}
 	public:
 		/* Helper functions */
 		bool has_key(const key_type& key) const
 		{
-			return m_tree.find(key) != m_tree.end();
+			return m_key_tree.find(key) != m_key_tree.end();
 		}
 		bool has_value(const value_type& value) const
 		{
-			return m_rtree.find(value) != m_rtree.end();
+			return m_value_tree.find(value) != m_value_tree.end();
 		}
 		const value_type& get_value(const key_type& key) const
 		{
-			return m_tree.at(key);
+			auto offset = index_of_key(key);
+			auto v_itr = m_value_tree.begin();
+			std::advance(v_itr, offset);
+			return *v_itr;
 		}
 		const key_type& get_key(const value_type& value) const
 		{
-			return m_rtree.at(value);
+			auto offset = index_of_value(value);
+			auto k_itr = m_key_tree.begin();
+			std::advance(k_itr, offset);
+			return *k_itr;
 		}
 		size_t size() const
 		{
-			if (m_tree.size() != m_rtree.size()) 
+			if (m_key_tree.size() != m_value_tree.size())
 				throw(std::exception("Tree sizes do not match!"));
-			return m_tree.size();
+			return m_key_tree.size();
 		}
 	};
 
@@ -104,9 +150,8 @@ namespace stde
 		class _KeyType,
 		class _ValueType>
 		using unordered_bimap = bimap<
-			_KeyType, 
-			_ValueType,
-			std::unordered_map<_KeyType, _ValueType>,
-			std::unordered_map<_ValueType, _KeyType>>;
-
+		_KeyType,
+		_ValueType,
+		std::unordered_set<_KeyType>,
+		std::unordered_set<_ValueType>>;
 }
